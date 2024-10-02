317235e9f9e824a72b6a1336955c72fcf9dfb53c
sebastianwyz <sebastian.waruszynski@studenti.unipd.it>
2024-10-02 15:32:39 +0200

    sparse_glm.py

--- svise/variationalsparsebayes/sparse_glm.py
+++ svise/variationalsparsebayes/sparse_glm.py
@@ -2,11 +2,12 @@ from abc import ABC, abstractmethod
 from .svi_half_cauchy import SVIHalfCauchyPrior
 import torch
 import torch.nn as nn
+import itertools  # Aggiungi questa linea per importare itertools
 from torch import Tensor
 from torch.nn.parameter import Parameter
 import math
 from typing import Tuple, Dict, Union, List, Iterable, Callable
-from itertools import chain, combinations_with_replacement, tee
+from itertools import chain, combinations_with_replacement, tee, combinations
 import numpy as np
 
 
@@ -123,19 +124,139 @@ class Polynomial:
     """
     Class for computing polynomial feature of a given degree
     """
-
+    
     def __init__(self, ind) -> None:
         self.ind = ind
 
     def __call__(self, x: Tensor) -> Tensor:
         return x[..., self.ind].prod(-1)
+    '''
+    def __init__(self, dim: int) -> None:
+        if isinstance(dim, tuple):
+            raise ValueError("Dimension must be a single integer, not a tuple")
+        self.dim = dim
 
-
+    def __call__(self, x: Tensor) -> Tensor:
+        # Calcola il seno delle differenze tra tutte le coppie di elementi in x
+        results = []
+        for i in range(self.dim):
+            for j in range(i + 1, self.dim):
+                sin_diff = torch.sin(x[..., i] - x[..., j]).unsqueeze(-1)
+                results.append(sin_diff)
+        # Concatena tutti i risultati lungo l'ultima dimensione per ottenere un singolo tensore
+        return torch.cat(results, dim=-1)
+    '''
 class SparsePolynomialFeatures(SparseFeaturesLibrary):
     """
     Polynomial features library for use with sparse models. Draws heavily from scikit learn implementation.
     """
+    '''
+    def __init__(
+        self,
+        dim: int,
+        degree: int,
+        include_bias: bool = True,
+        input_labels: List[str] = None,
+    ):
+        super().__init__()
+        self.dim = dim
+        self.degree = degree
+        self.include_bias = include_bias
+        
+        # Compute labels for polynomial and sinusoidal terms
+        ############################ nagumo #######################
+        labels = []
+        N = int(dim / 2)
+        for i in range(1, N + 1):
+            for var in ['u']:
+                for power in range(1, 4):
+                    labels.append(f"{var}{i}**{power}")
 
+        for i in range(1, N + 1):
+            for ver in ['v']:
+                labels.append(f"{ver}{i}")
+
+        for i in range(1, N + 1):
+            for j in range(i + 1, N + 1):
+                labels.append(f"sin(v{i}-v{j})")
+
+        self.input_labels = labels if input_labels is None else input_labels
+        self.__feature_names = self.input_labels
+        self.__num_features = len(self.__feature_names)
+        self.register_buffer("sparse_index", torch.arange(self.num_features))
+        ##############################################################
+        
+        labels = []
+        N = int(dim)
+
+        for i in range(0, N):
+            for j in range(i + 1, N):
+                labels.append(f"sin(theta_{i} - theta{j})")
+        print(labels)
+        self.input_labels = labels if input_labels is None else input_labels
+        self.__feature_names = self.input_labels
+        self.__num_features = len(self.__feature_names)
+        self.register_buffer("sparse_index", torch.arange(self.num_features))
+
+
+    @property
+    def feature_names(self) -> List[str]:
+        return self.__feature_names
+
+    @feature_names.setter
+    def feature_names(self, value):
+        self.__feature_names = value
+
+    @property
+    def num_features(self) -> int:
+        return self.__num_features
+
+    @num_features.setter
+    def num_features(self, value):
+        self.__num_features = value
+
+    def update_basis(self, sparse_index: Tensor) -> None:
+        assert (
+            self.num_features >= sparse_index.max()
+        ), "More sparsity inducing indices than features."
+        self.sparse_index = sparse_index
+    
+    ######################### nagumo ########################
+    def forward(self, x: Tensor) -> Tensor:
+        terms = []
+        N = int(x.shape[-1] / 2)
+        for i in range(0, N):
+            for power in range(1, 4):
+                term = x[..., i] ** power
+                terms.append(term.unsqueeze(-1))
+        for j in range(0, N):
+            term = x[..., N + j]
+            terms.append(term.unsqueeze(-1))
+
+        for i in range(0, N):
+            for j in range(i + 1, N):
+                term = torch.sin(x[..., 2 * i + 1] - x[..., 2 * j + 1])
+                terms.append(term.unsqueeze(-1))
+
+        x_transformed = torch.cat(terms, dim=-1)
+        return x_transformed
+    ##########################################################
+
+    def forward(self, x: Tensor) -> Tensor:
+        terms = []
+        N = int(x.shape[-1])
+
+        for i in range(0, N):
+            for j in range(i + 1, N):
+                term = torch.sin(x[..., i] - x[..., j])
+                terms.append(term.unsqueeze(-1))
+
+        x_transformed = torch.cat(terms, dim=-1)
+        return x_transformed
+    def __str__(self) -> str:
+        return " + ".join(self.feature_names)
+
+'''
     def __init__(
         self,
         dim: int,
@@ -147,10 +268,15 @@ class SparsePolynomialFeatures(SparseFeaturesLibrary):
         self.dim = dim
         self.degree = degree
         start = int(not include_bias)
+        
+
+        #iter_comb = ((i,) * n for i in range(dim) for n in range(start, degree + 1))
+            
         iter_comb = chain.from_iterable(
             combinations_with_replacement(range(dim), i)
             for i in range(start, degree + 1)
         )
+        
         # create two copies of iterable
         iter_comb_1, iter_comb_2 = tee(iter_comb)
         # array of functions
@@ -201,9 +327,11 @@ class SparsePolynomialFeatures(SparseFeaturesLibrary):
         Returns:
             Tensor: sparse dictionary evaluated at input (..., num_features[sparse_index])
         """
+        
         return torch.stack(
-            [f_i(x) for f_i in self.full_polynomial[self.sparse_index]], dim=-1
-        )
+           [f_i(x) for f_i in self.full_polynomial[self.sparse_index]], dim=-1)
+        
+
 
     def __str__(self) -> str:
         return " + ".join(self.feature_names[self.sparse_index])
@@ -218,6 +346,8 @@ class SparsePolynomialFeatures(SparseFeaturesLibrary):
         Returns:
             List[str]: list of strings containing feature names
         """
+
+        
         powers = np.vstack([np.bincount(c, minlength=self.dim) for c in iter_comb])
         feature_names = []
         for row in powers:
@@ -233,14 +363,16 @@ class SparsePolynomialFeatures(SparseFeaturesLibrary):
                 name = "1"
             feature_names.append(name)
         return np.array(feature_names)
-
+        
 
 # TODO: unit test this class
+
 class SparsePolynomialSinusoidTfm(SparsePolynomialFeatures):
     """
     Feature library that includes polynomials and sinusoids transform of input features.
     ex: ["x", "y"] -> ["1", "x", "y", "sin(x)", "cos(x)", "sin(y)", "cos(y)", "x^2", "y^2", ... ]
-
+    
+    l'idea ora è di includere sin(x-y),sin(z-y)...
     """
 
     def __init__(
@@ -251,15 +383,115 @@ class SparsePolynomialSinusoidTfm(SparsePolynomialFeatures):
         input_labels: List[str] = None,
     ):
         # note, I don't think we need a freq + phase argument as we can always scale x?
+        ''' #QUESTA PARTE SI AGGIUNGE PER INCLUDERE INTERAZIONI TRA LE VARIABILI DELLO STESSO NODO  
         if input_labels is None:
             input_labels = ["x_{}".format(i) for i in range(dim)]
         else:
             input_labels = [el for el in input_labels]
-        sin_labels = [f"sin({il})" for il in input_labels]
-        cos_labels = [f"cos({il})" for il in input_labels]
-        input_labels += sin_labels + cos_labels
-        super().__init__(dim * 3, degree, include_bias, input_labels)
+        #sin_labels = [f"sin({il})" for il in input_labels]
+        #cos_labels = [f"cos({il})" for il in input_labels]
+        combined_sin_labels = []
+        combined_cos_labels = []  
+        for i in range(len(input_labels)):
+            for j in range(i + 1, len(input_labels)):
+                combined_sin_labels.append(f"sin({input_labels[i]} + {input_labels[j]})")
+                combined_sin_labels.append(f"sin({input_labels[i]} - {input_labels[j]})")
+                #combined_cos_labels.append(f"cos({input_labels[i]} + {input_labels[j]})")
+                #combined_cos_labels.append(f"cos({input_labels[i]} - {input_labels[j]})")
+        
+        if input_labels is None:
+            print("non ci sono input labels")
+            #----------------------- caso generale, la dinamica di un nodo può dipendere da qualsiasi variabile di un altro nodo-------
+            #input_labels = ["x_{}".format(i) for i in range(dim)] + ["y_{}".format(i) for i in range(dim)] + ["z_{}".format(i) for i in range(dim)]
+            #--------------------------------------------------------------------------------------------------------------------------
+            primo_input_labels = ["x_0","y_0","z_0"]
+        else:
+            input_labels = [el for el in input_labels]
+        primo_input_labels = ["x_0","y_0","z_0"]
+        '''
+        ################################################### LORENZ #######################################################################
+        '''
 
+        combined_sin_labels = []
+
+        for coord_index in range(3):  # 0, 1, 2 corrispondono a x, y, z
+            for i in range(coord_index, dim, 3):  # parte da una coordinata specifica e scorre solo le coordinate simili nelle triplette successive
+                for j in range(i + 3, dim, 3):  # assicura il confronto solo tra gli stessi tipi di coordinate (es. x con x)
+                    if i // 3 != j // 3:  # non necessario in questo caso specifico dato il range, ma lo lasciamo per completezza
+                        combined_sin_labels.append(f"sin({input_labels[i]} - {input_labels[j]})")
+
+        input_labels += combined_sin_labels
+        print("input labels",len(input_labels))
+        print("input labels",(input_labels))
+        super().__init__((len(input_labels)), degree, include_bias, input_labels)
+
+        '''
+        '''
+        ###################################################     FITZHIGH-NAGUMO     ################################################################################
+        
+        labels = []
+        N = int(dim/2)
+        for i in range(1, N+1):
+            for var in ['u']: 
+                for power in range(1, 4):
+                    label = f"{var}{i}**{power}"
+                    labels.append(label)
+
+        for i in range(1, N+1):
+            for ver in ['v']: 
+                label = f"{ver}{i}"
+                labels.append(label)
+
+        for i in range(1, N+1):
+            for j in range(i+1, N+1):
+                #labels.append(f"sin(u{i}-u{j})")
+                labels.append(f"sin(v{i}-v{j})")
+        print(labels)
+        super().__init__(dim=len(labels), degree=degree, include_bias=True, input_labels=labels)
+
+        ###################################################################################################################################
+        '''
+        '''
+        ############################################## LORENZ MATTO #######################################################################
+        combined_sin_labels = []
+
+        # Fissiamo i = 1 per ottenere solo le differenze che coinvolgono la coordinata 1
+        i = 1
+        for j in range(i + 3, dim, 3):  # Inizia da i+3 e incrementa di 3 fino alla fine dell'array
+            combined_sin_labels.append(f"sin({input_labels[i]} - {input_labels[j]})")
+
+        # Aggiungi le nuove etichette al set esistente di input_labels
+        input_labels += combined_sin_labels
+
+        # Output per verificare il numero e il contenuto delle etichette
+        print("input labels", len(input_labels))
+        print("input labels", (input_labels))
+
+        # Assumendo che la classe inizializzata sia parte di un contesto più grande
+        super().__init__(len(input_labels), degree, include_bias, input_labels)
+
+        ###################################################################################################################################
+
+        '''
+        '''
+        ################################################## KURAMOTO #######################################################################
+        combined_sin_labels = []
+        #dim = 1 # PROVA CON ANALISI DI UN SOLO NODO
+        for i in range(dim):  # parte da una coordinata specifica e scorre solo le coordinate simili nelle triplette successive
+                for j in range(i + 1, dim):  # assicura il confronto solo tra gli stessi tipi di coordinate (es. x con x)
+                        combined_sin_labels.append(f"sin({input_labels[i]} - {input_labels[j]})")
+
+        input_labels += combined_sin_labels
+        #print("input labels",len(input_labels))
+        #print("input labels",(input_labels))
+        
+        #super().__init__((len(combined_sin_labels)), degree, include_bias=False, combined_sin_labels) #### HO INCLUSO FALSE 2323232
+        '''
+        #super().__init__(dim=len(combined_sin_labels), degree=degree, include_bias=True, input_labels=combined_sin_labels)
+        
+        super().__init__(dim=len(input_labels), degree=degree, include_bias=True, input_labels=input_labels)
+
+        ###################################################################################################################################
     def forward(self, x: Tensor) -> Tensor:
         """
         Computes the sparse dictionary of basis functions output
@@ -270,9 +502,104 @@ class SparsePolynomialSinusoidTfm(SparsePolynomialFeatures):
         Returns:
             Tensor: sparse dictionary evaluated at input (..., num_features[sparse_index])
         """
-        x_in = torch.cat([x, torch.sin(x), torch.cos(x)], dim=-1)
+        
+        #terms_to_concat = [x] 
+
+
+        #-----------------------------DINAMICA POLINOMIALE PER UN NODO E KURAMOTO INTERNODI-------------------------------
+       
+        #terms_to_concat = [x[..., i].unsqueeze(-1) for i in range(3)]
+        #-----------------------------------------------------------------------------------------------------------------
+        terms_to_concat = []
+
+        
+        ##################################################### LORENZ #####################################################################
+        '''
+        
+
+         
+        
+        for coord_index in range(3):  # 0, 1, 2 corrispondono a x, y, z         
+            for i in range(coord_index, x.shape[-1] , 3):  # parte da una coordinata specifica e scorre solo le coordinate simili nelle triplette successive
+                for j in range(i + 3, x.shape[-1], 3):  # assicura il confronto solo tra gli stessi tipi di coordinate (es. x con x)
+                    if i // 3 != j // 3:  # non necessario in questo caso specifico dato il range, ma lo lasciamo per completezza         
+                        terms_to_concat.append(torch.sin(x[..., i] - x[..., j]).unsqueeze(-1))
+
+        terms_to_concat = torch.cat(terms_to_concat, dim=-1)  # Concatena tutti i tensori nella lista lungo l'ultima dimensione
+        x_in = torch.cat([x,terms_to_concat], dim=-1)
+
+        '''
+        ################################################################################################################################## 
+        
+        #####################################################   FITZHUGH-NAGUMO    #####################################################################
+        N = int(int(x.shape[-1])/2)
+        terms = []
+        for i in range(0, N):
+            for power in range(1, 4):
+                term =   x[...,i]**power       
+                terms.append(term.unsqueeze(-1))
+        for j in range(0,N):
+            term = x[...,N+j]
+            terms.append(term.unsqueeze(-1))
+        
+        #N = int(int(x.shape[-1])/2)
+        
+        #for i in range(0, N):
+        #    for j in range(i+1, N):
+                #term1 = torch.sin(x[..., 2 * i] - x[..., 2 * j])
+        #        term2 = torch.sin(x[..., 2 * i + 1] - x[..., 2 * j + 1])
+        #        #terms.append(term1.unsqueeze(-1))
+        #        terms.append(term2.unsqueeze(-1))
+        
+        x_in = torch.cat(terms, dim=-1)
+
         return super().forward(x_in)
 
+        ################################################################################################################################## 
+        
+        
+        '''
+        ################################################# LORENZ MATTO ###################################################################
+        terms_to_concat = []
+
+        # Fissiamo i = 1 per ottenere solo le differenze che coinvolgono la coordinata 1
+        i = 1
+        for j in range(i + 3, x.shape[-1], 3):  # Inizia da i+3 e incrementa di 3 fino alla fine dell'array
+            terms_to_concat.append(torch.sin(x[..., i] - x[..., j]).unsqueeze(-1)) 
+
+        terms_to_concat = torch.cat(terms_to_concat, dim=-1)  # Concatena tutti i tensori nella lista lungo l'ultima dimensione
+        x_in = torch.cat([x,terms_to_concat], dim=-1)
+        '''
+        
+        '''
+        ####################################################### KURAMOTO #################################################################
+
+        half_dim = int(x.shape[-1])  #  /2         l'ho toltoAssicurati che sia un intero
+        for i in range(half_dim): #(x.shape[-1]/2):  # parte da una coordinata specifica e scorre solo le coordinate simili nelle triplette successive
+            for j in range(i,half_dim):
+                if i!=j:  # Inizia j da i + 1 per evitare duplicati e autoreferenze
+                    sin_difference = torch.sin(x[..., i] - x[..., j]).unsqueeze(-1)
+                    terms_to_concat.append(sin_difference)
+        #ones_tensor = torch.tensor([0.5539, -0.3248, -0.8373, -0.1117]) ################aggiunta 232323    
+        #ones_tensor = ones_tensor.unsqueeze(1)  # Aggiunge una nuova dimensione a dim=1
+        
+        #terms_to_concat = torch.cat([x,terms_to_concat], dim=-1)  ############# senza x credo che faccia forward di sinsinsisnisnin....(xi-xj)
+        if terms_to_concat:
+            terms_to_concat = torch.cat(terms_to_concat, dim=-1)  # Concatena tutti i tensori nella lista lungo l'ultima dimensione
+            x_in = terms_to_concat
+            #x_in = torch.cat([ones_tensor, terms_to_concat], dim=-1)  # Concatena il tensore di 1, x, e i termini calcolati
+            #x_in = torch.cat([x, terms_to_concat], dim=-1)  # Concatena lungo l'ultima dimensione
+        else:
+            print("errore in sparse glm sinusoid tfm") #x_in = torch.cat([ones_tensor, x], dim=-1)  # Se non ci sono termini di seno, concatena solo 1 e x
+        '''
+        
+        
+# return torch.stack(
+#          [f_i(x) for f_i in self.full_polynomial[self.sparse_index]], dim=-1       )
+        #x_in = torch.cat([x,terms_to_concat], dim=-1)
+        #################################################################################################################################
+        return super().forward(x_in)
+        
 
 # TODO: unit test this class
 class SparsePolynomialNeighbour1D(SparsePolynomialFeatures):
